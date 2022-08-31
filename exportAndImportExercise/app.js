1ef5f5bb7e641ebed3e5bb805f9db5122cc1653e
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-08-31 10:46:40 -0300

    add

---
+++ exportAndImportExercise/app.js
@@ -0,0 +1,21 @@
+import showArray from "./helpers/showArray.js";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const numbersPlus = (increaseValue, values) => values.map((e) => e + increaseValue);
+const filterEvens = (values) => values.filter((e) => e % 2 === 0);
+const filterOdds = (values) => values.filter((e) => e % 2 !== 0);
+
+const applyDiscount = (discount, values) => values.map((e) => e - ((e / 100) * discount))
+const totalValue = (values) => values.reduce((a, b) => a + b, 0);
+
+
+showArray(numbers, 'main', 'Todos os Numeros:')
+
+showArray(numbersPlus(100, numbers), 'main', 'Aumentando 100 em cada elemento');
+
+showArray(filterEvens(numbers), 'main', 'Somento os numeros pares');
+
+showArray([totalValue(applyDiscount(4, numbers)).toFixed(3)],'main', 'Total dos valores com desconto')
+
+showArray(numbersPlus(21, filterOdds(numbers)),'main', 'todos os elementos impares somados de 21')
--- exportsAndImports/app.js
+++ exportsAndImports/app.js
@@ -1,6 +1,17 @@
 import showArray from "./helpers/showArray.js";
 
-// container to be included the arrays 
-const numbers = [ 23, 5, 8, 10, 100, 45 ]
+// Container to be included the arrays 
+const numbers = [23, 5, 8, 10, 100, 45]
 
-showArray(numbers, 'main');
+// Functions to manipulate the numbers array
+const applyDiscount = (numbers, percentage) => numbers.map((e) => e - ((e / 100) * percentage));
+const filterByLowerThan = (values, targetValue) => values.filter((e) => e >= targetValue);
+const totalValue = (values) => values.reduce((a, b) => a + b, 0);
+
+showArray(numbers, 'main', `todo os items: `);
+
+showArray(applyDiscount(numbers, 20), 'main', `items com desconto de 20% `);
+
+showArray(filterByLowerThan(numbers, 10), 'main', `todo os items maiores que 10: `);
+
+showArray([totalValue(numbers)], 'main', 'Valor Total: ')
