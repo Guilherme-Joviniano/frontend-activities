85295125e39456eb98c261f0d930cdf6ee394dd8
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-09-14 10:40:55 -0300

    timeout error card increased

--- search-dogs/app.js
+++ search-dogs/app.js
@@ -1,14 +1,24 @@
 import fetchImagesDog from "./src/components/Gallery/fetchDogsImages.js";
-import { createGallery, createImageCards } from "./src/components/Gallery/index.js";
-import { showMessage } from './src/utils/showMessage.js'
+import {
+    createGallery,
+    createImageCards
+} from "./src/components/Gallery/index.js";
+import {
+    showMessage
+} from './src/utils/showMessage.js'
 const inputSearch = document.querySelector(`input`);
 
-const handleSearch = async ({ key, target }) => {
+const handleSearch = async ({
+    key,
+    target
+}) => {
     if (key === 'Enter') {
         const response = await fetchImagesDog(target.value);
         console.log(response);
-        if (response.status === 'error') return showMessage('No Founded Hound', 4000) // 4s 
-        const { message } = response; 
+        if (response.status === 'error') return showMessage('No Founded Hound', 8000) // 8s 
+        const {
+            message
+        } = response;
         const images = createImageCards(message);
         createGallery(images)
     }
